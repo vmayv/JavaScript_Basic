d1d8ff824d0d8b5e16f91087c0c7a788ac964fab
vmayv <vasyamayorov@yandex.ru>
2021-07-21 21:54:09 +0300

    Выполнено задание 3. Поправлены задания 1 и 2

--- hw05/task02.js
+++ hw05/task02.js
@@ -4,7 +4,7 @@ function CreateChessBoardWithPieces() {
   var pawn = "П";
   var task3 = document.getElementById("Задание 3");
   var table = document.createElement("table");
-  var isBlack = false;
+  var isBlack = true;
   var whiteBoardColor = "#ABABAB";
   var blackBoardColor = "#797979";
   var whiteColor = "white";
@@ -13,48 +13,47 @@ function CreateChessBoardWithPieces() {
   table.border = 1;
 
   for (var i = 1; i < 10; i++) {
-    var tr = document.createElement("tr");
+    var tr = document.createElement("tr");// создаём строки
     for (var j = 1; j < 10; j++) {
-      var td = document.createElement("td");
+      var td = document.createElement("td");// создаём столбцы
       td.style.backgroundColor = whiteBoardColor;
-      if (i === 9 && j > 1) {
+      if (i === 9 && j > 1) { // строка с буквенными именованием
         td.innerText = `${literals[j - 2]}`;
         td.style.backgroundColor = whiteColor;
       }
-      if (i < 9 && j === 1) {
+      if (i < 9 && j === 1) { // строка с численным именованием
         td.innerText = `${9 - i}`;
         td.style.backgroundColor = whiteColor;
       }
-      if (j > 1 && i < 9 && !isBlack) {
-        // разобраться!!! написал и сам не понял!!
+      if (j > 1 && i < 9 && isBlack) { // через один красим в чёрный
         td.style.backgroundColor = blackBoardColor;
       }
       if (i === 9 && j === 1) {
         // угловой нижний элемент делаем белым
         td.style.backgroundColor = whiteColor;
       }
-      if (i === 2 && j > 1) {
+      if (i === 2 && j > 1) { // чёрные пешки
         td.innerText = pawn;
         td.style.color = blackColor;
         td.align = "center";
       }
-      if (i === 7 && j > 1) {
+      if (i === 7 && j > 1) { // белые пешки
         td.innerText = pawn;
         td.style.color = whiteColor;
         td.align = "center";
       }
-      if (i === 1 && j > 1) {
+      if (i === 1 && j > 1) { // чёрные фигуры
         td.innerText = pieces[j - 2];
         td.style.color = blackColor;
         td.align = "center";
       }
-      if (i === 8 && j > 1) {
+      if (i === 8 && j > 1) { // белые фигуры
         td.innerText = pieces[j - 2];
         td.align = "center";
         td.style.color = whiteColor;
       }
-      td.width = 30;
-      td.height = 30;
+      td.width = 45;
+      td.height = 45;
       isBlack = !isBlack;
       tr.appendChild(td);
     }
--- hw05/task03.js
+++ hw05/task03.js
@@ -1,39 +1,78 @@
-var item = {
-  type: "clothes",
-  sex: "male",
-  category: "shorts",
-  brand: "nike",
-  colour: "black",
-  size: { rus: "", uk: "", us: "" }, // можно что-то ещё придумать с размерами: высота, ширина и тд.
-  leftInStock: 10,
-  amount: 15,
-  price: 2000,
-  countryOfProduction: "China",
-  article: "908397423694X",
-  composition: { cotton: 50, polyester: 50 }, // можно расширить
-  inBasket: true,
-  countInBasket: 1,
+function CreateChessBoardWithPics() {
+  var literals = ["A", "B", "C", "D", "E", "F", "G", "H"];
+  var pieces = [
+    "svg/rook_{color}.svg",
+    "svg/knight_{color}.svg",
+    "svg/bishop_{color}.svg",
+    "svg/queen_{color}.svg",
+    "svg/king_{color}.svg",
+    "svg/bishop_{color}.svg",
+    "svg/knight_{color}.svg",
+    "svg/rook_{color}.svg",
+  ];
+  var task3 = document.getElementById("Задание 3");
+  var table = document.createElement("table");
+  var isBlack = true;
+  var whiteBoardColor = "#ABABAB";
+  var blackBoardColor = "#797979";
+  var whiteColor = "white";
+  var blackColor = "black";
+  table.title = "Шахматная доска";
+  table.border = 1;
 
-  AddToBasket: function () {
-    if (this.leftInStock === 0) {
-      alert("Нет на складе!");
-      return;
-    }
-
-    if (!this.inBasket) {
-      this.inBasket = true;
-    }
-    this.leftInStock -= 1;
-    this.countInBasket += 1;
-  },
-  RemoveFromBasket: function () {
-    if (this.inBasket && this.countInBasket > 0) {
-      this.countInBasket -= 1;
-      this.leftInStock += 1;
-
-      if (this.countInBasket === 0) {
-        this.inBasket = false;
+  for (var i = 1; i < 10; i++) {
+    var tr = document.createElement("tr");
+    for (var j = 1; j < 10; j++) {
+      var td = document.createElement("td");
+      td.style.backgroundColor = whiteBoardColor;
+      if (i === 9 && j > 1) {
+        td.innerText = `${literals[j - 2]}`;
+        td.style.backgroundColor = whiteColor;
       }
+      if (i < 9 && j === 1) {
+        td.innerText = `${9 - i}`;
+        td.style.backgroundColor = whiteColor;
+      }
+      if (j > 1 && i < 9 && isBlack) { // через один красим в чёрный
+        td.style.backgroundColor = blackBoardColor;
+      }
+      if (i === 9 && j === 1) {
+        // угловой нижний элемент делаем белым
+        td.style.backgroundColor = whiteColor;
+      }
+      if (i === 2 && j > 1) { // чёрные пешки
+        var img = document.createElement("img");
+        img.src = './svg/pawn_black.svg'
+        td.appendChild(img);
+        td.align = "center";
+      }
+      if (i === 7 && j > 1) { // белые пешки
+        var img = document.createElement("img");
+        img.src = './svg/pawn_white.svg'
+        td.appendChild(img);
+        td.align = "center";
+      }
+
+      if (i === 1 && j > 1) { // чёрные фигуры
+        var img = document.createElement("img");
+        img.src = pieces[j - 2].replace('{color}', 'black');
+        td.appendChild(img);
+        td.style.color = blackColor;
+        td.align = "center";
+      }
+      if (i === 8 && j > 1) { // белые фигуры
+        var img = document.createElement("img");
+        img.src = pieces[j - 2].replace('{color}', 'white');
+        td.appendChild(img);
+        td.align = "center";
+        td.style.color = whiteColor;
+      }
+      td.width = 45;
+      td.height = 45;
+      isBlack = !isBlack;
+      tr.appendChild(td);
     }
-  },
-};
+    table.appendChild(tr);
+  }
+  document.body.appendChild(table);
+}
