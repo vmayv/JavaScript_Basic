558eef8770f00fdf613cbd88b638c3b32c0dc935
vmayv <vasyamayorov@yandex.ru>
2021-07-26 23:26:36 +0300

    Начато дз 6

--- hw06/tasks01-02.js
+++ hw06/tasks01-02.js
@@ -1,27 +1,81 @@
-var basket = {
+var store = {
+  basket: [],
   items: [
     {
-      id: "1",
-      group: "group1",
-      name: "item1",
-      description: "test",
-      price: 5.2,
+      type: "clothes",
+      sex: "male",
+      category: "shorts",
+      brand: "nike",
+      colour: "black",
+      size: { rus: "", uk: "", us: "" }, // можно что-то ещё придумать с размерами: высота, ширина и тд.
+      leftInStock: 1,
+      amount: 15,
+      price: 2000,
+      countryOfProduction: "China",
+      article: "908397423694X",
+      composition: { cotton: 50, polyester: 50 }, // можно расширить
+      inBasket: true,
+      countInBasket: 1,
     },
     {
-      id: "2",
-      group: "group1",
-      name: "item2",
-      description: "test",
-      price: 10.6,
+      type: "clothes",
+      sex: "male",
+      category: "shorts",
+      brand: "nike",
+      colour: "black",
+      size: { rus: "", uk: "", us: "" }, // можно что-то ещё придумать с размерами: высота, ширина и тд.
+      leftInStock: 10,
+      amount: 15,
+      price: 1200,
+      countryOfProduction: "China",
+      article: "908397423695X",
+      composition: { cotton: 50, polyester: 50 }, // можно расширить
+      inBasket: true,
+      countInBasket: 1,
     },
     {
-      id: "3",
-      group: "group2",
-      name: "item3",
-      description: "test",
-      price: 20.5,
+      type: "clothes",
+      sex: "male",
+      category: "shorts",
+      brand: "nike",
+      colour: "black",
+      size: { rus: "", uk: "", us: "" }, // можно что-то ещё придумать с размерами: высота, ширина и тд.
+      leftInStock: 10,
+      amount: 15,
+      price: 200,
+      countryOfProduction: "China",
+      article: "908397423696X",
+      composition: { cotton: 50, polyester: 50 }, // можно расширить
+      inBasket: true,
+      countInBasket: 1,
     },
   ],
+
+  AddToBasket: function (item) {
+    // if (this.items.leftInStock === 0) {
+    //   alert("Нет на складе!");
+    //   return;
+    // }
+
+    // if (!this.items.inBasket) {
+    //   this.items.inBasket = true;
+    // }
+    // this.items.leftInStock -= 1;
+    // this.items.countInBasket += 1;
+
+    store.basket.push(item);
+  },
+
+  RemoveFromBasket: function () {
+    if (this.inBasket && this.countInBasket > 0) {
+      this.countInBasket -= 1;
+      this.leftInStock += 1;
+
+      if (this.countInBasket === 0) {
+        this.inBasket = false;
+      }
+    }
+  },
 
   BasketPrice: function () {
     var count = 0;
@@ -39,43 +93,48 @@ var basket = {
     alert(result);
   },
 };
-var item = {
-  type: "clothes",
-  sex: "male",
-  category: "shorts",
-  brand: "nike",
-  colour: "black",
-  size: { rus: "", uk: "", us: "" }, // можно что-то ещё придумать с размерами: высота, ширина и тд.
-  leftInStock: 10,
-  amount: 15,
-  price: 2000,
-  countryOfProduction: "China",
-  article: "908397423694X",
-  composition: { cotton: 50, polyester: 50 }, // можно расширить
-  inBasket: true,
-  countInBasket: 1,
 
-  AddToBasket: function () {
-    if (this.leftInStock === 0) {
-      alert("Нет на складе!");
-      return;
-    }
+function renderItems() {
+  var items = document.querySelector("#items-container");
+  var ul = document.createElement("ul");
 
-    if (!this.inBasket) {
-      this.inBasket = true;
-    }
-    this.leftInStock -= 1;
-    this.countInBasket += 1;
-  },
-  RemoveFromBasket: function () {
-    if (this.inBasket && this.countInBasket > 0) {
-      this.countInBasket -= 1;
-      this.leftInStock += 1;
+  for (var i = 0; i < store.items.length; i++) {
+    const li = document.createElement("li");
+    li.style.marginBottom = "20px";
 
-      if (this.countInBasket === 0) {
-        this.inBasket = false;
-      }
-    }
-  },
-};
+    const span = document.createElement("span");
+    const img = document.createElement("img");
+    img.src = "https://thiscatdoesnotexist.com";
+    img.style.width = "100px";
 
+    const article = document.createElement("p");
+    article.innerText = store.items[i].article;
+
+    const name = document.createElement("p");
+    name.innerText = `${store.items[i].category} ${store.items[i].brand}`;
+
+    const price = document.createElement("p");
+    price.innerText = store.items[i].price;
+
+    const button = document.createElement("button");
+    button.innerText = "Add";
+    button.id = "AddToBasket";
+
+    span.appendChild(img);
+    span.appendChild(article);
+    span.appendChild(name);
+    span.appendChild(price);
+    span.appendChild(button);
+    li.appendChild(span);
+    ul.appendChild(li);
+    
+  }
+  items.appendChild(ul);
+
+  var buttons = document.querySelectorAll("#AddToBasket");
+  for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", () => store.AddToBasket(store.items[i]));
+  }
+
+}
+renderItems();
