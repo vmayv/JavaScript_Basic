b1ab26cd790fa1e768cfd7fa05311016b3d1cd36
vmayv <vasyamayorov@yandex.ru>
2021-08-23 23:42:22 +0300

    Функции привязал к объектам

--- hw06/tasks01-02.js
+++ hw06/tasks01-02.js
@@ -51,6 +51,87 @@ const store = {
     },
   ],
 
+  renderItems() {
+    const items = document.querySelector("#items-container");
+    const ul = document.createElement("ul");
+
+    for (let i = 0; i < store.items.length; i++) {
+      const li = document.createElement("li");
+      li.style.marginBottom = "20px";
+
+      const span = document.createElement("span");
+      const img = document.createElement("img");
+      img.src = "https://thiscatdoesnotexist.com";
+      img.style.width = "100px";
+
+      const article = document.createElement("p");
+      article.innerText = store.items[i].article;
+
+      const name = document.createElement("p");
+      name.innerText = `${store.items[i].category} ${store.items[i].brand}`;
+
+      const price = document.createElement("p");
+      price.innerText = store.items[i].price;
+
+      const button = document.createElement("button");
+      button.innerText = "Add";
+      button.id = "AddToBasket";
+
+      span.appendChild(img);
+      span.appendChild(article);
+      span.appendChild(name);
+      span.appendChild(price);
+      span.appendChild(button);
+      li.appendChild(span);
+      ul.appendChild(li);
+
+    }
+    items.appendChild(ul);
+
+    const buttons = document.querySelectorAll("#AddToBasket");
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].addEventListener("click", () => {
+        const item = store.items[i];
+        store.AddToBasket(item);
+      });
+    }
+  },
+
+  renderBasket() {
+    const items = document.querySelector("#basket-container");
+
+    const ul = document.createElement("ul");
+    const basketEntries = Object.entries(store.basket);
+
+    if (basketEntries.length > 0) {
+      ul.style.border = '2px green solid';
+      ul.style.backgroundColor = 'lightblue';
+    }
+
+    for (let i = 0; i < basketEntries.length; i++) {
+      const [article, count] = basketEntries[i];
+      const item = store.items.filter(e => e.article === article)[0];
+
+      const li = document.createElement("li");
+      li.style.marginBottom = "20px";
+      li.style.borderBottom = "2px solid black";
+
+      const spanContainer = document.createElement("span");
+      spanContainer.innerText = `${item.article} ${item.category} ${item.brand} - ${count}- ${count * item.price}`;
+
+      const removeButton = document.createElement("button");
+      removeButton.style.float = 'right'
+      removeButton.addEventListener("click", () => store.RemoveFromBasket(item));
+      removeButton.textContent = 'x';
+
+      li.appendChild(spanContainer);
+      li.appendChild(removeButton);
+      ul.appendChild(li);
+    }
+
+    items.replaceChild(ul, items.firstChild);
+  },
+
   AddToBasket: function (item) {
     if (item.leftInStock === 0) {
       alert("Нет на складе!");
@@ -63,7 +144,7 @@ const store = {
 
     item.leftInStock -= 1;
     store.basket[item.article] += 1
-    renderBasket();
+    store.renderBasket();
 
   },
 
@@ -77,7 +158,7 @@ const store = {
     //     this.inBasket = false;
     //   }
     // }
-    renderBasket();
+    store.renderBasket();
   },
 
   BasketPrice: function () {
@@ -97,87 +178,5 @@ const store = {
   },
 };
 
-function renderItems() {
-  const items = document.querySelector("#items-container");
-  const ul = document.createElement("ul");
-
-  for (let i = 0; i < store.items.length; i++) {
-    const li = document.createElement("li");
-    li.style.marginBottom = "20px";
-
-    const span = document.createElement("span");
-    const img = document.createElement("img");
-    img.src = "https://thiscatdoesnotexist.com";
-    img.style.width = "100px";
-
-    const article = document.createElement("p");
-    article.innerText = store.items[i].article;
-
-    const name = document.createElement("p");
-    name.innerText = `${store.items[i].category} ${store.items[i].brand}`;
-
-    const price = document.createElement("p");
-    price.innerText = store.items[i].price;
-
-    const button = document.createElement("button");
-    button.innerText = "Add";
-    button.id = "AddToBasket";
-
-    span.appendChild(img);
-    span.appendChild(article);
-    span.appendChild(name);
-    span.appendChild(price);
-    span.appendChild(button);
-    li.appendChild(span);
-    ul.appendChild(li);
-
-  }
-  items.appendChild(ul);
-
-  const buttons = document.querySelectorAll("#AddToBasket");
-  for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", () => {
-      const item = store.items[i];
-      store.AddToBasket(item);
-    });
-  }
-
-}
-
-function renderBasket() {
-  const items = document.querySelector("#basket-container");
-
-  const ul = document.createElement("ul");
-  const basketEntries = Object.entries(store.basket);
-
-  if (basketEntries.length > 0) {
-    ul.style.border = '2px green solid';
-    ul.style.backgroundColor = 'lightblue';
-  }
-
-  for (let i = 0; i < basketEntries.length; i++) {
-    const [article, count] = basketEntries[i];
-    const item = store.items.filter(e => e.article === article)[0];
-
-    const li = document.createElement("li");
-    li.style.marginBottom = "20px";
-    li.style.borderBottom = "2px solid black";
-
-    const spanContainer = document.createElement("span");
-    spanContainer.innerText = `${item.article} ${item.category} ${item.brand} - ${count}- ${count * item.price}`;
-
-    const removeButton = document.createElement("button");
-    removeButton.style.float = 'right'
-    removeButton.addEventListener("click", () => store.RemoveFromBasket(item));
-    removeButton.textContent = 'x';
-
-    li.appendChild(spanContainer);
-    li.appendChild(removeButton);
-    ul.appendChild(li);
-  }
-
-  items.replaceChild(ul, items.firstChild);
-}
-
-renderItems();
-renderBasket();
+store.renderItems();
+store.renderBasket();
